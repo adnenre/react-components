78f83f6d6c54c29f80673582404b1b13447bed7b
adnen rebai <adnen.rebai@gmail.com>
2023-10-04 04:56:48 +0200

    add themeProvider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
+
 import Main from './pages/Main';
 import SideBarre from './SideBarre';
 
 import './styles.css';
 import { BrowserRouter } from 'react-router-dom';
 import Toggler from './components/Toggler';
+import theme from './theme';
 
 const App = () => {
   const [isActive, setActive] = useState(false);
@@ -14,13 +17,15 @@ const App = () => {
     setActive(!isActive);
   };
   return (
-    <BrowserRouter>
-      <div className="App">
-        <SideBarre active={isActive} />
-        <Main active={isActive} />
-        <Toggler onClick={activateSideBar} />
-      </div>
-    </BrowserRouter>
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <div className="App">
+          <SideBarre active={isActive} />
+          <Main active={isActive} />
+          <Toggler onClick={activateSideBar} />
+        </div>
+      </BrowserRouter>
+    </ThemeProvider>
   );
 };
 
