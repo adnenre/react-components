6c4acb752d6cd2c477263a660429b1536937bfc8
adnen rebai <adnen.rebai@gmail.com>
2023-10-06 09:36:16 +0200

    enhance the Tree component

--- src/components/Tree.js
+++ src/components/Tree.js
@@ -1,11 +1,17 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import theme from '../theme';
 
+const ParentNode = styled.div`
+  cursor: pointer; /* Add cursor style to indicate interactivity */
+  display: block; /* Display nodes horizontally */
+`;
 const TreeNode = styled.div`
   cursor: pointer; /* Add cursor style to indicate interactivity */
-  display: inline-block; /* Display nodes horizontally */
-  margin-left: 20px; /* Adjust the indentation as needed */
+  display: flex; /* Display nodes horizontally */
+  justify-content: left;
+  margin-left: 10px; /* Adjust the indentation as needed */
 `;
 
 const ExpandedTreeNode = styled(TreeNode)`
@@ -14,7 +20,7 @@ const ExpandedTreeNode = styled(TreeNode)`
 `;
 
 const TreeLeaf = styled.span`
-  color: green; /* Style the leaves differently */
+  color: ${theme.colors.$black}; /* Style the leaves differently */
 `;
 
 const Caret = styled.span`
@@ -24,11 +30,13 @@ const Caret = styled.span`
 const Tree = ({ data }) => {
   const [expandedNodes, setExpandedNodes] = useState({});
 
-  const toggleExpansion = (nodeId) => {
-    setExpandedNodes((prevState) => ({
-      ...prevState,
-      [nodeId]: !prevState[nodeId],
-    }));
+  const toggleExpansion = (node) => {
+    if (node.children) {
+      setExpandedNodes((prevState) => ({
+        ...prevState,
+        [node.id]: !prevState[node.id],
+      }));
+    }
   };
 
   const renderTree = (node) => {
@@ -38,17 +46,17 @@ const Tree = ({ data }) => {
     const NodeComponent = isExpanded ? ExpandedTreeNode : TreeNode;
 
     return (
-      <div key={node.id}>
-        <NodeComponent onClick={() => toggleExpansion(node.id)}>
+      <ParentNode key={node.id}>
+        <NodeComponent onClick={() => toggleExpansion(node)}>
           {hasChildren && <Caret>{isExpanded ? '▼' : '▶'}</Caret>}
           {node.label}
         </NodeComponent>
         {isExpanded && hasChildren && (
-          <div style={{ textIndent: '20px' }}>
+          <TreeLeaf style={{ textIndent: node.level * 20 + 'px' }}>
             {node.children.map((child) => renderTree(child))}
-          </div>
+          </TreeLeaf>
         )}
-      </div>
+      </ParentNode>
     );
   };
 
