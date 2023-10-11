d0d42e98550c703cdc8ff808a9b9673711695163
Adnen Rebai <adnen.rebai@gmail.com>
2023-10-11 03:10:07 +0200

    add view code section to tooltip page

--- src/pages/TooltipPage.js
+++ src/pages/TooltipPage.js
@@ -4,6 +4,10 @@ import PortletView from '../components/Portlet';
 import Tooltip from '../components/Tooltip';
 import Button from '../components/Button';
 import { Col, Container, Row } from '../components/grid';
+import 'prismjs/themes/prism.css';
+import PrismCode from 'react-prism';
+import Prism from 'prismjs';
+import Toggle from '../components/Toggle';
 
 const TooltipPage = () => {
   return (
@@ -37,6 +41,38 @@ const TooltipPage = () => {
             </Row>
           </Container>
         </>
+      }
+      footer={
+        <Toggle>
+          <PrismCode component="pre" className="language-markup" target>
+            {`
+          <Row>
+          <Col $md={3}>
+            <Tooltip text="Tooltip on the left " $left>
+              <Button $primary> Hover me ! </Button>
+            </Tooltip>
+          </Col>
+          <Col $md={3}>
+            <Tooltip text="Tooltip on the bottom">
+              <Button $primary> Hover me ! </Button>
+            </Tooltip>
+          </Col>
+          <Col $md={3}>
+            <Tooltip text="Tooltip on the right" $right>
+              <Button $primary> Hover me ! </Button>
+            </Tooltip>
+          </Col>
+
+          <Col $md={3}>
+            <Tooltip text="Tooltip on the Top" $top>
+              <Button $primary> Hover me ! </Button>
+            </Tooltip>
+          </Col>
+        </Row>
+          
+          `}
+          </PrismCode>
+        </Toggle>
       }
     />
   );
