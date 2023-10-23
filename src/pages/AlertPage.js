18e6593aef766e76a93361614de1bf7429a5a5dd
Adnen Rebai <adnen.rebai@gmail.com>
2023-10-23 06:25:42 +0200

    refactor : Alert page

--- src/pages/AlertPage.js
+++ src/pages/AlertPage.js
@@ -3,18 +3,102 @@ import PortletView from '../components/Portlet';
 import Alert from '../components/Alert';
 import fake_data from '../fake-data';
 
+import 'prismjs/themes/prism.css';
+import PrismCode from 'react-prism';
+import Prism from 'prismjs';
+import Toggle from '../components/Toggle';
+import { Container, Row } from '../components/grid';
+
 const AlertPage = () => {
   return (
-    <PortletView
-      title={fake_data.pages.alert.page.title}
-      content={
-        <Alert
-          type="primary"
-          title={fake_data.pages.alert.title}
-          desc={fake_data.pages.alert.desc}
-        />
-      }
-    />
+    <Container>
+      <PortletView
+        title={fake_data.pages.alert.page.title}
+        content={
+          <Container>
+            <Row>
+              <Alert
+                $info
+                title={'Info'}
+                description={fake_data.pages.alert.desc}
+              />
+              <Alert
+                $success
+                title={'Success'}
+                description={fake_data.pages.alert.desc}
+              />
+              <Alert
+                $danger
+                title={'Danger'}
+                description={fake_data.pages.alert.desc}
+              />
+              <Alert
+                $warning
+                title={'Warning'}
+                description={fake_data.pages.alert.desc}
+              />
+            </Row>
+          </Container>
+        }
+        footer={
+          <Toggle>
+            <PrismCode component="pre" className="language-markup" target>
+              {`
+            <Alert $info    title='Info'     description={fake_data.pages.alert.desc} />
+            <Alert $success title='Success'  description={fake_data.pages.alert.desc} />
+            <Alert $danger  title='Danger'   description={fake_data.pages.alert.desc} />
+            <Alert $warning title='Warning'  description={fake_data.pages.alert.desc} />
+          `}
+            </PrismCode>
+          </Toggle>
+        }
+      />
+      <PortletView
+        title={fake_data.pages.alert.page.title}
+        content={
+          <Container>
+            <Row>
+              <Alert
+                $pill
+                $info
+                title={'Info'}
+                description={fake_data.pages.alert.desc}
+              />
+              <Alert
+                $pill
+                $success
+                title={'Success'}
+                description={fake_data.pages.alert.desc}
+              />
+              <Alert
+                $pill
+                $danger
+                title={'Danger'}
+                description={fake_data.pages.alert.desc}
+              />
+              <Alert
+                $pill
+                $warning
+                title={'Warning'}
+                description={fake_data.pages.alert.desc}
+              />
+            </Row>
+          </Container>
+        }
+        footer={
+          <Toggle>
+            <PrismCode component="pre" className="language-markup" target>
+              {`
+            <Alert $pill $info    title='Info'     description={fake_data.pages.alert.desc} />
+            <Alert $pill $success title='Success'  description={fake_data.pages.alert.desc} />
+            <Alert $pill $danger  title='Danger'   description={fake_data.pages.alert.desc} />
+            <Alert $pill $warning title='Warning'  description={fake_data.pages.alert.desc} />
+          `}
+            </PrismCode>
+          </Toggle>
+        }
+      />
+    </Container>
   );
 };
 
