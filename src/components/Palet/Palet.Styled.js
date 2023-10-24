14203cfd2b0200ba53277a5cc9d661b44d2f93eb
Adnen Rebai <adnen.rebai@gmail.com>
2023-10-24 04:33:37 +0200

    fix : Palet responsive

--- src/components/Palet/Palet.Styled.js
+++ src/components/Palet/Palet.Styled.js
@@ -6,6 +6,9 @@ export const PaletWrapper = styled.div`
   justify-content: center;
   align-items: center;
   gap: 0.5rem;
+  @media screen and (max-width: 1100px) {
+    gap: 0;
+  }
 `;
 export const PaletContainer = styled.div`
   display: flex;
@@ -17,15 +20,28 @@ export const PaletBox = styled.div`
   display: flex;
 
   align-items: flex-end;
-  width: 100%;
-  min-width: 80px;
+  width: 70px;
   height: 80px;
   background: ${({ $color }) => $color};
-  margin: 0.2rem;
-  border-radius: 10px;
+
   transition: 0.2s;
   cursor: pointer;
-
+  @media screen and (min-width: 1000px) {
+    width: 100%;
+    min-width: 120px;
+  }
+  @media screen and (min-width: 769) and( max-width: 1000px) {
+    width: 100%;
+    min-width: 50px;
+    height: 50px;
+    margin: 0;
+  }
+  @media screen and (max-width: 768px) {
+    width: 30px;
+    min-width: 20px;
+    height: 30px;
+    margin: 5px;
+  }
   &:hover {
     box-shadow:
       rgba(0, 0, 0, 0.1) 0rem 0.625rem 0.9375rem -0.1875rem,
@@ -37,4 +53,7 @@ export const PaletCode = styled.h5`
   background: white;
   text-align: center;
   font-size: 0.75rem;
+  @media screen and (max-width: 768px) {
+    display: none;
+  }
 `;
