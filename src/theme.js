d57656a41a1b8f4499b9c607504bc9d13d14372e
Adnen Rebai <adnen.rebai@gmail.com>
2023-10-24 03:47:25 +0200

    feat : add background property to theme

--- src/theme.js
+++ src/theme.js
@@ -42,6 +42,21 @@ const theme = {
     $grayDark: '#94a2bd',
     ...base.colors,
   },
+  background: {
+    $white: '#ffffff',
+    $default: '#FFF',
+    $primary: '#3a81f7',
+    $secondary: '#AB47BC',
+    $info: '#5bc0de',
+    $success: '#99cc54',
+    $warning: '#f0ad4e',
+    $danger: '#d9534f',
+    $black: '#434A54',
+    $gray: '#2e3f6b',
+    $grayLight: '#e4edfe', //'#f3f4f6',
+    $grayDark: '#94a2bd',
+    ...base.colors,
+  },
 
   font: {
     $color: '#001b41',
