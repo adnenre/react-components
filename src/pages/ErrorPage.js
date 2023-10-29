ecaa07c5aa70319b9c122c71d3cca340b010e1e2
Adnen Rebai <adnen.rebai@gmail.com>
2023-10-29 03:55:33 +0100

    feat : ErrorPage

---
+++ src/pages/ErrorPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useRouteError } from 'react-router-dom';
+
+const ErrorContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+`;
+export default function ErrorPage() {
+  const error = useRouteError();
+  console.error(error);
+
+  return (
+    <ErrorContainer>
+      <h1>Oops!</h1>
+      <p>Sorry, an unexpected error has occurred.</p>
+      <p>
+        <i>{error.statusText || error.message}</i>
+      </p>
+    </ErrorContainer>
+  );
+}
